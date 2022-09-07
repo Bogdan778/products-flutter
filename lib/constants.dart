f9d4f6134c783a224e7979d2ecad2850bd9da17b
bogdan778 <leyn778@gmail.com>
2022-09-07 19:02:00 +0300

    added snackbar

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 final passwordFieldDecoration = InputDecoration(
-    border: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(30.0),
-    ),
-    prefixIcon: const Icon(
-      Icons.security,
-      color: Colors.black,
-    ),
-    hintText: 'Please enter your password');
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(30.0),
+  ),
+  prefixIcon: const Icon(
+    Icons.security,
+    color: Colors.black,
+  ),
+  hintText: 'Please enter your password',
+);
 
 final emailFieldDecoration = InputDecoration(
     border: OutlineInputBorder(
@@ -21,3 +22,7 @@ final emailFieldDecoration = InputDecoration(
     hintText: 'Please enter your email');
 
 const textStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.w500);
+
+const snackBarMessage = SnackBar(
+  content: Text('Wrong email or password!'),
+);
