0f6b64fe437e1202a6007c13bbc80426b672ae6a
bogdan778 <leyn778@gmail.com>
2022-09-12 14:46:09 +0300

    keep user logged in

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:products_test_flutter/ui/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:products_test_flutter/ui/login/login_page.dart';
+import 'package:products_test_flutter/ui/product/product_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  final bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+  runApp(ProviderScope(child: MyApp(isLoggedIn: isLoggedIn)));
+}
+
+class MyApp extends StatelessWidget {
+  final bool isLoggedIn;
+  const MyApp({Key? key, required this.isLoggedIn}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print(isLoggedIn);
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: isLoggedIn ? const ProductPage() : const LoginPage());
+  }
 }
