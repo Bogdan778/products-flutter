14e1bf198d89b1f6bb803d4c2636da93b1a00611
bogdan778 <leyn778@gmail.com>
2022-09-12 14:45:39 +0300

    keep user logged in

--- lib/ui/login/login_page.dart
+++ lib/ui/login/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:products_test_flutter/ui/product/product_screen.dart';
-import 'package:products_test_flutter/utils/snackbar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../constants.dart';
 
@@ -16,12 +16,15 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     final emailController = TextEditingController();
     final passwordController = TextEditingController();
-    void _auth() {
+
+    void _auth() async {
       final email = emailController.text;
       final password = passwordController.text;
       if (email == 'fflabel@ff.f' && password == 'fflabel') {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => const ProductPage()));
+        final prefs = await SharedPreferences.getInstance();
+        prefs.setBool('IsLoggedIn', true);
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => ProductPage()));
       } else {
         ScaffoldMessenger.of(context).showSnackBar(snackBarMessage);
       }
@@ -29,54 +32,54 @@ class _LoginPageState extends State<LoginPage> {
 
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 150,
-            ),
-            const Text(
-              'Email',
-              style: textStyle,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              decoration: emailFieldDecoration,
-              controller: emailController,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              'Password',
-              style: textStyle,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              decoration: passwordFieldDecoration,
-              controller: passwordController,
-              obscureText: true,
-            ),
-            const SizedBox(
-              height: 170,
-            ),
-            SizedBox(
-              height: 60,
-              width: 350,
-              child: ElevatedButton(
-                onPressed: _auth,
-                child: const Text('Sign In'),
-              ),
-            )
-          ],
-        ),
-      ),
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 150,
+                ),
+                const Text(
+                  'Email',
+                  style: textStyle,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  decoration: emailFieldDecoration,
+                  controller: emailController,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  'Password',
+                  style: textStyle,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  decoration: passwordFieldDecoration,
+                  controller: passwordController,
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 170,
+                ),
+                SizedBox(
+                  height: 60,
+                  width: 350,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      _auth();
+                    },
+                    child: const Text('Sign In'),
+                  ),
+                ),
+              ])),
     );
   }
 }
--- lib/ui/product/product_screen.dart
+++ lib/ui/product/product_screen.dart
@@ -1,14 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:products_test_flutter/ui/login/login_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ProductPage extends StatelessWidget {
+import '../../models/product_model.dart';
+import '../../providers/products_data_provider.dart';
+
+class ProductPage extends ConsumerWidget {
   const ProductPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    controller:
+    ScrollController();
+    final _data = ref.watch(productsDataProvider);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('List of product'),
+        title: const Text(
+          'List of product',
+        ),
+        centerTitle: true,
       ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Your account'),
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  final prefs = await SharedPreferences.getInstance();
+                  prefs.setBool('IsLoggedIn', false);
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: ((context) => LoginPage()),
+                  ));
+                },
+                child: Text('Logout'))
+          ],
+        ),
+      ),
+      body: _data.when(
+          data: (_data) {
+            List<ProductsModel> productsList = _data.map((e) => e).toList();
+            return Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                      controller: ScrollController(),
+                      itemCount: productsList.length,
+                      itemBuilder: (_, index) {
+                        return Card(
+                            color: Colors.orange,
+                            elevation: 4,
+                            child: ListTile(
+                              title: Text(productsList[index].body),
+                            ));
+                      }),
+                )
+              ],
+            );
+          },
+          error: (err, s) => Text(err.toString()),
+          loading: () => const Center(
+                child: CircularProgressIndicator(),
+              )),
     );
   }
 }
